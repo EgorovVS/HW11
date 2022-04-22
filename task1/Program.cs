230e8c4c3bf3fe3f75f487a8c606447cbbd79c99
Spoon2 <viktor.spoon2@gmail.com>
2022-04-23 00:28:52 +0400

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 1: Определите, является ли число степенью двойки:
+N = 16 -> "Является степень двойки"
+N = 12 -> “Не является степенью двойки” */
+
+int N = Prompt(" Введите число ");
+PowerTwo(N);
+
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+void PowerTwo(int num, int sqr = 2)
+{
+    if (num == 1)
+    {
+        Console.WriteLine($"{N} является степенью двойки");
+        return;
+    }
+    if (num % sqr == 0)
+    {
+        PowerTwo(num / 2, sqr);
+    }
+    else
+    {
+        {
+            Console.WriteLine($"{N} не является степенью двойки");
+            return;
+        }
+    }
+}
